e71f52178d33b8ed0707d4ab933a63799e3d429d
Till W <theasuro@gmail.com>
2016-03-02 18:21:49 +0100

    Draw multiple objects

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 var canvas = document.getElementById("canvas");
 var gl = canvas.getContext("experimental-webgl");
 
+var drawQueue = [];
+
 function loadShaderFromId(id, type)
 {
 	var shader = gl.createShader(type);
@@ -65,19 +67,58 @@ function loadFloatArrayBuffer(shaderProgram, array, varName)
 	return buffer;
 }
 
+function createRectObject(vertId, fragId, x, y, width, height)
+{
+	var vs = loadShaderFromId(vertId, gl.VERTEX_SHADER);
+	var fs = loadShaderFromId(fragId, gl.FRAGMENT_SHADER);
+	var shader = loadShaderProgram(vs, fs);
+
+	var resolutionPointer = gl.getUniformLocation(shader, "u_resolution");
+	gl.uniform2f(resolutionPointer, canvas.width, canvas.height);
+
+	return {
+		vertexShader: vs,
+		fragmentShader: fs,
+		shader: shader,
+		texCoords: loadFloatArrayBuffer(shader, createTextureCoordArray(), "a_uv"),
+		worldCoords: loadFloatArrayBuffer(shader, createRectangleArray(x, y, width + x, height + y), "a_position"),
+		xPos: x,
+		yPos: y,
+		width: width,
+		height: height
+	}
+}
+
+function draw()
+{
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+	for (var key in drawQueue)
+	{
+		gl.bindBuffer(gl.ARRAY_BUFFER, drawQueue[key].worldCoords);
+
+		var bufferPointer = gl.getAttribLocation(drawQueue[key].shader, "a_position");
+		gl.enableVertexAttribArray(bufferPointer);
+		gl.vertexAttribPointer(bufferPointer, 2, gl.FLOAT, false, 0, 0);
+
+		gl.drawArrays(gl.TRIANGLES, 0, 6);
+	}
+	
+	window.requestAnimationFrame(draw);
+}
+
 function initialize()
 {
-	// Load shaders
-	var vertexShader = loadShaderFromId("vert", gl.VERTEX_SHADER);
-	var fragmentShader = loadShaderFromId("frag-cicle", gl.FRAGMENT_SHADER);
-	var shader = loadShaderProgram(vertexShader, fragmentShader);
+	gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
 	// Initialize objects
-	var texCoords = loadFloatArrayBuffer(shader, createTextureCoordArray(), "a_uv");
-	var worldCoords = loadFloatArrayBuffer(shader, createRectangleArray(0,0,1,1), "a_position");
+	var testCircle0 = createRectObject("vert", "frag-circle", 0, 0, 100, 100);
+	var testCircle1 = createRectObject("vert", "frag-circle", 100, 100, 100, 100);
+	var testCircle2 = createRectObject("vert", "frag-circle", 200, 200, 100, 100);
+
+	drawQueue = [testCircle0, testCircle1, testCircle2];
+
+	draw();
 }
 
 initialize();
-
-// Draw
-gl.drawArrays(gl.TRIANGLES, 0, 6);
