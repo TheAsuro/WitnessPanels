2bdad7aee81ba87b1e1e4179ffd1c299d302e338
Till W <theasuro@gmail.com>
2016-03-02 22:10:24 +0100

    Moved general engine code to main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 var canvas = document.getElementById("canvas");
 canvas.width = document.body.clientWidth;
 canvas.height = document.body.clientHeight;
-var gl = canvas.getContext("webgl", { alpha: false });
+var gl = canvas.getContext("webgl", { alpha: false, antialias: true });
 window.addEventListener("resize", function(event) {
 	canvas.width = document.body.clientWidth;
 	canvas.height = document.body.clientHeight;
@@ -94,6 +94,54 @@ function createRectObject(vertId, fragId, x, y, width, height)
 		height: height
 	}
 }
+
+function relMouseCoords(event)
+{
+    var totalOffsetX = 0;
+    var totalOffsetY = 0;
+    var canvasX = 0;
+    var canvasY = 0;
+    var currentElement = this;
+
+    do
+    {
+        totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
+        totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+    }
+    while(currentElement = currentElement.offsetParent)
+
+    canvasX = event.pageX - totalOffsetX;
+    canvasY = event.pageY - totalOffsetY;
+
+    return {x:canvasX, y:canvasY}
+}
+
+HTMLCanvasElement.prototype.relMouseCoords = relMouseCoords;
+
+function createGameObject(vertId, fragId, x, y, width, height, clickEvent)
+{
+	var obj = createRectObject(vertId, fragId, x, y, width, height);
+	obj.clickEvent = clickEvent;
+	drawQueue.push(obj);
+}
+
+canvas.addEventListener("click", function(event)
+{
+	var x = canvas.relMouseCoords(event).x;
+	var y = canvas.relMouseCoords(event).y;
+
+	for (var key in drawQueue)
+	{
+		if (drawQueue[key].x <= x && x <= drawQueue[key].x + drawQueue[key].width
+			&& (canvas.height - drawQueue[key].y) - drawQueue[key].height <= y && y <= (canvas.height - drawQueue[key].y))
+		{
+			if (drawQueue[key].clickEvent !== undefined)
+			{
+				drawQueue[key].clickEvent();
+			}
+		}
+	}
+});
 
 function draw()
 {
